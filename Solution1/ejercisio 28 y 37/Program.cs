a1c0264e67804d43b71b96c7ac5ed91b60816053
JHONATAN JARAMILLO GOMEZ <jhonatanjaramillo1158857@correo.itm.edu.co>
2026-03-03 16:34:12 -0500

    ejercisio 28

---
+++ Solution1/ejercisio 28 y 37/Program.cs
@@ -0,0 +1,49 @@
+﻿
+class Ejercicio28
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Ingrese la viga: ");
+
+        char viga = Console.ReadKey().KeyChar;  // Lee un solo carácter
+        Console.WriteLine(); // Salto de línea
+
+        if (EsValida(viga))
+        {
+            if (SoportaPeso(viga))
+            {
+                Console.WriteLine("La viga soporta el peso!");
+            }
+            else
+            {
+                Console.WriteLine("La viga no soporta el peso");
+            }
+        }
+        else
+        {
+            Console.WriteLine("La viga esta mal construida");
+        }
+    }
+
+    static bool EsValida(char viga)
+    {
+        // Si es alguno de estos caracteres, no es válida
+        if (viga == '*' || viga == 'T' || viga == '=')
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    static bool SoportaPeso(char viga)
+    {
+        // Ejemplo de condición (puedes modificarla)
+        if (viga == 'H')
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
